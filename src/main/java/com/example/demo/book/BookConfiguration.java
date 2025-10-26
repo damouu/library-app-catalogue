570c9c36eda2bb819891a490b0a868dd9fda2f27
damouu <mouadsehbaoui@gmail.com>
2025-10-26 15:43:00 +0100

    #SD-229 Done Unassigned TestsPassed Done
    Refactor: Book.java

--- src/main/java/com/example/demo/book/BookConfiguration.java
+++ src/main/java/com/example/demo/book/BookConfiguration.java
@@ -8,16 +8,16 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.UUID;
 
-@Configuration
+//@Configuration
 public class BookConfiguration {
     //    @Bean
-    CommandLineRunner dedede(BookRepository bookRepository) {
-        return args -> {
-            Faker faker = new Faker();
-            for (int i = 1; i < 100; i++) {
-                Book book = new Book(UUID.randomUUID(), faker.book().title(), faker.book().genre(), faker.number().numberBetween(1, 900), faker.book().publisher(), faker.book().author(), LocalDate.of(faker.number().numberBetween(1900, 2021), Month.NOVEMBER, faker.number().numberBetween(1, 31)));
-                bookRepository.save(book);
-            }
-        };
-    }
+//    CommandLineRunner dedede(BookRepository bookRepository) {
+//        return args -> {
+//            Faker faker = new Faker();
+//            for (int i = 1; i < 100; i++) {
+//                Book book = new Book(UUID.randomUUID(), faker.book().title(), faker.book().genre(), faker.number().numberBetween(1, 900), faker.book().publisher(), faker.book().author(), LocalDate.of(faker.number().numberBetween(1900, 2021), Month.NOVEMBER, faker.number().numberBetween(1, 31)));
+//                bookRepository.save(book);
+//            }
+//        };
+//    }
 }
