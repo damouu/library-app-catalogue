06063b93f6a432070f0aee54365c7428187a6cfd
damouu <mouadsehbaoui@gmail.com>
2025-10-27 11:54:06 +0100

    #SD-232 Done Unassigned TestsPassed Done
    refactor: MemberCardService.java
    
    creates function to borrow Books
    in the service layer.
    
    Reviewed-by: damou_ <mouadsehbaoui@gmail.com>

--- src/main/java/com/example/demo/memberCard/MemberCardService.java
+++ src/main/java/com/example/demo/memberCard/MemberCardService.java
@@ -1,6 +1,9 @@
 package com.example.demo.memberCard;
 
+import com.example.demo.book.Book;
 import com.example.demo.book.BookMemberCard;
+import com.example.demo.book.BookRepository;
+import com.example.demo.book.BookStudentRepository;
 import lombok.Data;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -18,7 +22,11 @@ import java.util.*;
 @Data
 public class MemberCardService {
 
+    private final BookRepository bookRepository;
+
     private final MemberCardRepository memberCardRepository;
+
+    private final BookStudentRepository bookMemberCardRepository;
 
 
     public ResponseEntity<List<MemberCard>> getMemberCards(int page, int size) {
@@ -30,14 +38,14 @@ public class MemberCardService {
     public ResponseEntity<LinkedHashMap<String, Object>> getMemberCard(UUID memberCardUUID) throws ResponseStatusException {
         LinkedHashMap<String, Object> response = new LinkedHashMap<>();
         Optional<MemberCard> MemberCard = Optional.ofNullable(memberCardRepository.findMemberCardByUuid(memberCardUUID).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "student card does not exist")));
-        Set<BookMemberCard> book = MemberCard.get().getBooks();
+//        Set<Book> book = Collections.singleton(MemberCard.get().getBooks().get(0));
         LinkedHashMap<String, Object> data = new LinkedHashMap<>();
         LinkedHashMap<String, Integer> list = new LinkedHashMap<>();
-        list.put("book", book.size());
+//        list.put("book", book.size());
         response.put("status", "success");
         response.put("type", "Collection");
         response.put("size", list);
-        data.put("book", book);
+//        data.put("book", book);
         response.put("data", data);
         return ResponseEntity.status(200).body(response);
     }
@@ -55,6 +63,34 @@ public class MemberCardService {
         memberCard.setValid_until(LocalDateTime.now().plusYears(2));
         memberCardRepository.save(memberCard);
         return ResponseEntity.status(HttpStatus.CREATED).location(URI.create("http://localhost:8083/api/studentCard/" + memberCard.getMember_card_uuid())).body(memberCard);
+    }
+
+    public ResponseEntity<HashMap<String, String>> borrowBooks(UUID memberCardUUID, Map<Object, ArrayList<UUID>> booksArrayJson) throws ResponseStatusException {
+        MemberCard memberCard = memberCardRepository.findMemberCardByUuid(memberCardUUID).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "member card does not exist"));
+        UUID uuid = UUID.randomUUID();
+        for (UUID bookUUID : booksArrayJson.get("books_UUID")) {
+            if (!bookMemberCardRepository.existsByBookIsBorrowed(bookUUID)) {
+                BookMemberCard bookMemberCard = new BookMemberCard();
+                bookMemberCard.setBorrow_start_date(LocalDate.now());
+                bookMemberCard.setBorrow_end_date(LocalDate.now().plusWeeks(2));
+                bookMemberCard.setBorrow_uuid(uuid);
+                bookMemberCard.setMemberCard(memberCard);
+                Book book = bookRepository.findByUuid(bookUUID).get();
+                book.set_borrowed(true);
+                bookMemberCard.setBook(book);
+                bookRepository.save(book);
+                bookMemberCardRepository.save(bookMemberCard);
+            } else {
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "error");
+            }
+        }
+
+        HashMap<String, String> response = new HashMap<>();
+        response.put("message", booksArrayJson.get("books_UUID").size() + "冊の本は貸し出しされる完了です。");
+        response.put("return_borrow_date", String.valueOf(LocalDate.now().plusWeeks(2)));
+        response.put("start_borrow_date", String.valueOf(LocalDate.now()));
+        response.put("borrow_UUID", uuid.toString());
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
 
