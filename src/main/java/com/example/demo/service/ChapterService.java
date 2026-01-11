5b0280ad13738409c15a39d5bbf85ffd013b7ef6
damouu <mouadsehbaoui@gmail.com>
2026-01-11 22:21:31 +0100

    Refactor: ChapterService (#LACE-4)

--- src/main/java/com/example/demo/service/ChapterService.java
+++ src/main/java/com/example/demo/service/ChapterService.java
@@ -4,7 +4,6 @@ import com.example.demo.model.Chapter;
 import com.example.demo.repository.ChapterRepository;
 import com.example.demo.spec.ChapterSpecification;
 import com.example.demo.util.PaginationUtil;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,7 +18,6 @@ import java.time.LocalDate;
 import java.util.*;
 
 
-@Data
 @Service
 @RequiredArgsConstructor
 public class ChapterService {
@@ -31,10 +29,11 @@ public class ChapterService {
         LocalDate endOfWeek = LocalDate.now().with(DayOfWeek.SUNDAY);
         Pageable pageable = PaginationUtil.extractPage(allParams);
         String typeValue = Optional.ofNullable(allParams.get("type")).map(Object::toString).orElse(null);
+        Page<Chapter> chapters;
         switch (typeValue) {
             case ("recent"):
                 Specification<Chapter> specification = ChapterSpecification.publishedBetween(startOfWeek, endOfWeek);
-                Page<Chapter> chapters = chapterRepository.findAll(specification, pageable);
+                chapters = chapterRepository.findAll(specification, pageable);
                 return ResponseEntity.status(HttpStatus.OK).body(chapters);
             case null:
                 specification = ChapterSpecification.filterChapter(allParams);
