0be3b0d19e5539293f45f5ff13bf131f399e91c1
damouu <mouadsehbaoui@gmail.com>
2026-06-07 20:07:30 +0900

    Test: SeriesServiceTest (#LACE-17)

--- src/test/java/com/example/demo/unit/service/SeriesServiceTest.java
+++ src/test/java/com/example/demo/unit/service/SeriesServiceTest.java
@@ -1,9 +1,12 @@
 package com.example.demo.unit.service;
 
+import com.example.demo.dto.CreateSeriesRequest;
+import com.example.demo.dto.SeriesCreatedEvent;
 import com.example.demo.model.Chapter;
 import com.example.demo.model.Series;
 import com.example.demo.repository.ChapterRepository;
 import com.example.demo.repository.SeriesRepository;
+import com.example.demo.service.KafkaPayloadBuilderService;
 import com.example.demo.service.SeriesService;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.Assertions;
@@ -20,12 +23,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.instancio.Select.field;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -38,6 +44,12 @@ class SeriesServiceTest {
     @Mock
     private SeriesRepository seriesRepository;
 
+    @Mock
+    private KafkaPayloadBuilderService payloadBuilderService;
+
+    @Mock
+    private KafkaTemplate<UUID, Object> kafkaTemplate;
+
     @InjectMocks
     private SeriesService seriesService;
 
@@ -45,11 +57,16 @@ class SeriesServiceTest {
 
     Chapter chapter;
 
+    SeriesCreatedEvent createdEvent;
+
+    CreateSeriesRequest createSeriesRequest;
+
     @BeforeEach
     void setUp() {
-        series = Instancio.create(Series.class);
+        series = Instancio.of(Series.class).set(field("title"), "jojo").create();
         chapter = Instancio.create(Chapter.class);
-
+        createdEvent = Instancio.create(SeriesCreatedEvent.class);
+        createSeriesRequest = Instancio.of(CreateSeriesRequest.class).set(field(CreateSeriesRequest::getTitle), series.getTitle()).set(field(CreateSeriesRequest::getGenre), series.getGenre()).set(field(CreateSeriesRequest::getAuthor), series.getAuthor()).create();
     }
 
     @Test
@@ -106,5 +123,29 @@ class SeriesServiceTest {
         ResponseEntity<?> responseEntity = seriesService.getSeriesChapters(allParams, series.getUuid());
         Assertions.assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
         verify(chapterRepository, Mockito.times(1)).findAll(any(Specification.class), any(Pageable.class));
+    }
+
+    @Test
+    void createSeries_false_true_case() {
+        when(seriesRepository.existsByTitle(series.getTitle())).thenReturn(false);
+        when(payloadBuilderService.seriesCreatedEvent(any(Series.class), eq("SERIES_CREATED"), eq("library-app-catalogue-v1"), any(UUID.class))).thenReturn(createdEvent);
+        when(kafkaTemplate.send(anyString(), any(UUID.class), any())).thenReturn(null);
+        ResponseEntity<?> responseEntity = seriesService.createSeries(createSeriesRequest);
+        verify(payloadBuilderService, Mockito.times(1)).seriesCreatedEvent(any(Series.class), eq("SERIES_CREATED"), eq("library-app-catalogue-v1"), any(UUID.class));
+        verify(seriesRepository, Mockito.times(1)).existsByTitle(series.getTitle());
+        verify(seriesRepository).save(any(Series.class));
+        Assertions.assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+    }
+
+    @Test
+    void createSeries_false_false_case() {
+        when(seriesRepository.existsByTitle(series.getTitle())).thenReturn(true);
+        ResponseStatusException exception = Assertions.assertThrows(ResponseStatusException.class, () -> {
+            seriesService.createSeries(createSeriesRequest);
+        });
+        verify(payloadBuilderService, Mockito.times(0)).seriesCreatedEvent(any(Series.class), eq("SERIES_CREATED"), eq("library-app-catalogue-v1"), any(UUID.class));
+        verify(seriesRepository, Mockito.times(1)).existsByTitle(series.getTitle());
+        verify(seriesRepository, Mockito.times(0)).save(any(Series.class));
+        Assertions.assertTrue(exception.getStatus().isError());
     }
 }
