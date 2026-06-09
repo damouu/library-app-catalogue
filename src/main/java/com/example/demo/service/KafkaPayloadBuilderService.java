46d3dc3b63a9055526ee988b5449d99cb71afba7
damouu <mouadsehbaoui@gmail.com>
2026-06-09 17:50:10 +0900

    Test: KafkaPayloadBuilderService (#LACE-18)

--- src/main/java/com/example/demo/service/KafkaPayloadBuilderService.java
+++ src/main/java/com/example/demo/service/KafkaPayloadBuilderService.java
@@ -1,24 +1,45 @@
 package com.example.demo.service;
 
-import com.example.demo.dto.Metadata;
-import com.example.demo.dto.SeriesCreatedEvent;
-import com.example.demo.dto.SeriesCreatedEventData;
+import com.example.demo.dto.*;
+import com.example.demo.mapper.ChapterMapper;
+import com.example.demo.mapper.SeriesMapper;
+import com.example.demo.model.Chapter;
 import com.example.demo.model.Series;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaPayloadBuilderService {
+
+    private final SeriesMapper seriesEventMapper;
+
+    private final ChapterMapper chapterMapper;
 
     public SeriesCreatedEvent seriesCreatedEvent(Series series, String eventType, String sourceService, UUID eventUUID) {
 
-        SeriesCreatedEventData eventData = SeriesCreatedEventData.builder().series_uuid(series.getUuid()).title(series.getTitle()).genre(series.getGenre()).cover_artwork_url(series.getCoverArtworkUrl()).illustrator(series.getIllustrator()).publisher(series.getPublisher()).author(series.getAuthor()).first_print_publication_date(series.getFirstPrintPublicationDate().toString()).last_print_publication_date(series.getLastPrintPublicationDate().toString()).build();
+        SeriesCreatedEventData eventData = seriesEventMapper.toEventData(series);
 
-        Metadata metadataPayload = Metadata.builder().event_uuid(eventUUID).event_type(eventType).timestamp(LocalDateTime.now().toString()).source_service(sourceService).build();
+        Metadata metadataPayload = buildMetadata(eventType, sourceService, eventUUID);
 
         return SeriesCreatedEvent.builder().metadata(metadataPayload).data(eventData).build();
+    }
+
+    public ChapterCreatedEvent chapterCreatedEvent(Chapter chapter, String eventType, String sourceService, UUID eventUUID, Integer initial_copies_count) {
+
+        ChapterCreatedEventData chapterCreatedEventData = chapterMapper.toEventData(chapter, initial_copies_count);
+
+        Metadata metadataPayload = buildMetadata(eventType, sourceService, eventUUID);
+
+        return ChapterCreatedEvent.builder().metadata(metadataPayload).data(chapterCreatedEventData).build();
+    }
+
+    private Metadata buildMetadata(String eventType, String sourceService, UUID eventUUID) {
+
+        return Metadata.builder().event_uuid(eventUUID).event_type(eventType).source_service(sourceService).timestamp(LocalDateTime.now().toString()).build();
     }
 
 }
