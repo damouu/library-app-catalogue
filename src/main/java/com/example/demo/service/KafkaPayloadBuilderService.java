c0ee169bf8680e84d8f8a50b99905fba20d23e99
damouu <mouadsehbaoui@gmail.com>
2026-06-17 19:48:09 +0900

    Refactor: KafkaPayloadBuilderService (#LACE-19)

--- src/main/java/com/example/demo/service/KafkaPayloadBuilderService.java
+++ src/main/java/com/example/demo/service/KafkaPayloadBuilderService.java
@@ -20,25 +20,18 @@ public class KafkaPayloadBuilderService {
     private final ChapterMapper chapterMapper;
 
     public SeriesCreatedEvent seriesCreatedEvent(Series series, String eventType, String sourceService, UUID eventUUID) {
-
         SeriesCreatedEventData eventData = seriesEventMapper.toEventData(series);
-
         Metadata metadataPayload = buildMetadata(eventType, sourceService, eventUUID);
-
         return SeriesCreatedEvent.builder().metadata(metadataPayload).data(eventData).build();
     }
 
     public ChapterCreatedEvent chapterCreatedEvent(Chapter chapter, String eventType, String sourceService, UUID eventUUID, Integer initial_copies_count) {
-
         ChapterCreatedEventData chapterCreatedEventData = chapterMapper.toEventData(chapter, initial_copies_count);
-
         Metadata metadataPayload = buildMetadata(eventType, sourceService, eventUUID);
-
         return ChapterCreatedEvent.builder().metadata(metadataPayload).data(chapterCreatedEventData).build();
     }
 
     private Metadata buildMetadata(String eventType, String sourceService, UUID eventUUID) {
-
         return Metadata.builder().event_uuid(eventUUID).event_type(eventType).source_service(sourceService).timestamp(LocalDateTime.now().toString()).build();
     }
 
