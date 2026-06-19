c2ce5f30f0aec0dbe72fb39b97c60eb1a3bc3336
damouu <mouadsehbaoui@gmail.com>
2026-06-19 19:05:50 +0900

    Refactor: CreateSeriesRequest (#LACE-21)

--- src/main/java/com/example/demo/dto/CreateSeriesRequest.java
+++ src/main/java/com/example/demo/dto/CreateSeriesRequest.java
@@ -1,30 +1,15 @@
 package com.example.demo.dto;
 
-import lombok.*;
-
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class CreateSeriesRequest {
-
-    private String title;
-
-    private String genre;
-
-    private String coverArtworkUrl;
-
-    private String illustrator;
-
-    private String publisher;
-
-    private String author;
-
-    private LocalDate firstPrintPublicationDate;
-
-    private LocalDate lastPrintPublicationDate;
-
+public record CreateSeriesRequest(
+        String title,
+        String genre,
+        String coverArtworkUrl,
+        String illustrator,
+        String publisher,
+        String author,
+        LocalDate firstPrintPublicationDate,
+        LocalDate lastPrintPublicationDate
+) {
 }
