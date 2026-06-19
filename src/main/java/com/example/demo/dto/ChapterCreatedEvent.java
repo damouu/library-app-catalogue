40f003ad757485dd2d165726acf365e7d6eaa623
damouu <mouadsehbaoui@gmail.com>
2026-06-19 19:04:10 +0900

    Refactor: ChapterCreatedEvent (#LACE-21)

--- src/main/java/com/example/demo/dto/ChapterCreatedEvent.java
+++ src/main/java/com/example/demo/dto/ChapterCreatedEvent.java
@@ -1,19 +1,11 @@
 package com.example.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@Builder
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ChapterCreatedEvent {
+public record ChapterCreatedEvent(
+        Metadata metadata,
 
-    private Metadata metadata;
-
-    private ChapterCreatedEventData data;
-
-
+        ChapterCreatedEventData data
+) {
 }
