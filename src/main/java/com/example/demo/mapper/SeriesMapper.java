469e826ac7957b8255e9a89d1a450ed3dededaf6
damouu <mouadsehbaoui@gmail.com>
2026-06-19 19:08:46 +0900

    Refactor: SeriesMapper (#LACE-21)

--- src/main/java/com/example/demo/mapper/SeriesMapper.java
+++ src/main/java/com/example/demo/mapper/SeriesMapper.java
@@ -2,6 +2,7 @@ package com.example.demo.mapper;
 
 import com.example.demo.dto.CreateSeriesRequest;
 import com.example.demo.dto.SeriesCreatedEventData;
+import com.example.demo.dto.SeriesSummaryDTO;
 import com.example.demo.model.Series;
 import org.springframework.stereotype.Component;
 
@@ -9,32 +10,35 @@ import java.util.UUID;
 
 @Component
 public class SeriesMapper {
-    public SeriesCreatedEventData toEventData(Series series) {
 
-        return SeriesCreatedEventData.builder()
-                .series_uuid(series.getUuid())
-                .title(series.getTitle())
-                .genre(series.getGenre())
-                .author(series.getAuthor())
-                .illustrator(series.getIllustrator())
-                .publisher(series.getPublisher())
-                .cover_artwork_url(series.getCoverArtworkUrl())
-                .first_print_publication_date(series.getFirstPrintPublicationDate().toString())
-                .last_print_publication_date(series.getLastPrintPublicationDate().toString())
-                .build();
+    public SeriesCreatedEventData toEventData(Series series) {
+        return new SeriesCreatedEventData(
+                series.getUuid(),
+                series.getTitle(),
+                series.getGenre(),
+                series.getAuthor(),
+                series.getIllustrator(),
+                series.getPublisher(),
+                series.getCoverArtworkUrl(),
+                series.getFirstPrintPublicationDate().toString(),
+                series.getLastPrintPublicationDate().toString());
     }
 
     public Series toEntity(CreateSeriesRequest request) {
         return Series.builder()
                 .uuid(UUID.randomUUID())
-                .title(request.getTitle())
-                .genre(request.getGenre())
-                .coverArtworkUrl(request.getCoverArtworkUrl())
-                .illustrator(request.getIllustrator())
-                .publisher(request.getPublisher())
-                .author(request.getAuthor())
-                .firstPrintPublicationDate(request.getFirstPrintPublicationDate())
-                .lastPrintPublicationDate(request.getLastPrintPublicationDate())
+                .title(request.title())
+                .genre(request.genre())
+                .coverArtworkUrl(request.coverArtworkUrl())
+                .illustrator(request.illustrator())
+                .publisher(request.publisher())
+                .author(request.author())
+                .firstPrintPublicationDate(request.firstPrintPublicationDate())
+                .lastPrintPublicationDate(request.lastPrintPublicationDate())
                 .build();
+    }
+
+    public SeriesSummaryDTO toSummaryDto(Series series) {
+        return new SeriesSummaryDTO(series.getUuid(), series.getTitle(), series.getGenre(), series.getCoverArtworkUrl(), series.getAuthor(), series.getIllustrator(), series.getPublisher());
     }
 }
