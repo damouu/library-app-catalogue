fa44afa3ffc81adcce5b4575782d28b6152c2d80
damouu <mouadsehbaoui@gmail.com>
2026-06-19 19:05:41 +0900

    Refactor: CreateChapterRequest (#LACE-21)

--- src/main/java/com/example/demo/dto/CreateChapterRequest.java
+++ src/main/java/com/example/demo/dto/CreateChapterRequest.java
@@ -1,32 +1,18 @@
 package com.example.demo.dto;
 
-import lombok.*;
-
 import java.time.LocalDate;
 import java.util.UUID;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class CreateChapterRequest {
 
-    private UUID series_uuid;
-
-    private String title;
-
-    private String second_title;
-
-    private Integer chapter_number;
-
-    private Integer total_pages;
-
-    private String cover_artwork_url;
-
-    private String summary;
-
-    private Integer initial_copies_count;
-
-    private LocalDate publication_date;
+public record CreateChapterRequest(
+        UUID series_uuid,
+        String title,
+        String second_title,
+        Integer chapter_number,
+        Integer total_pages,
+        String cover_artwork_url,
+        String summary,
+        Integer initial_copies_count,
+        LocalDate publication_date
+) {
 }
