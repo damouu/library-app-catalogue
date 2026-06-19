782d5807709c7d73305a83f0f28090fcdb49a4e9
damouu <mouadsehbaoui@gmail.com>
2026-06-19 19:07:29 +0900

    Refactor: SeriesCreatedEvent (#LACE-21)

--- src/main/java/com/example/demo/dto/SeriesCreatedEvent.java
+++ src/main/java/com/example/demo/dto/SeriesCreatedEvent.java
@@ -1,19 +1,7 @@
 package com.example.demo.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@Builder
-@AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class SeriesCreatedEvent {
-
-    private Metadata metadata;
-
-    private SeriesCreatedEventData data;
-
-
+public record SeriesCreatedEvent(
+        Metadata metadata,
+        SeriesCreatedEventData data
+) {
 }
