868f9d970a6cc7e7fbe75bf2e9c16be68c1bb07f
damouu <mouadsehbaoui@gmail.com>
2026-06-20 13:42:38 +0900

    Refactor: ChapterRepository (#LACE-21)

--- src/main/java/com/example/demo/repository/ChapterRepository.java
+++ src/main/java/com/example/demo/repository/ChapterRepository.java
@@ -21,8 +21,5 @@ public interface ChapterRepository extends JpaRepository<Chapter, Integer>, JpaS
     @EntityGraph(attributePaths = {"series"})
     Optional<Chapter> findByUuidAndDeletedAtIsNull(UUID chapterUUID);
 
-    @EntityGraph(attributePaths = {"series"})
-    Page<Chapter> findBySeriesUuid(UUID chapterUUID, Pageable pageable);
-
     boolean existsBySeries_UuidAndChapterNumber(UUID seriesUuid, Integer chapterNumber);
 }
