2e38a15052a9602ae959848bf9d2f989d92296bc
damouu <mouadsehbaoui@gmail.com>
2026-06-20 13:41:12 +0900

    Refactor: ChapterController (#LACE-21)

--- src/main/java/com/example/demo/controller/ChapterController.java
+++ src/main/java/com/example/demo/controller/ChapterController.java
@@ -1,15 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ChapterFilterDTO;
+import com.example.demo.dto.ChapterSummaryDTO;
 import com.example.demo.dto.CreateChapterRequest;
 import com.example.demo.model.Chapter;
 import com.example.demo.service.ChapterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 import java.util.UUID;
 
 @Validated
@@ -21,13 +23,13 @@ public class ChapterController {
     private final ChapterService chapterService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getChapter(@RequestParam Map<String, ?> allParams) {
-        return chapterService.getChapters(allParams);
+    public Page<ChapterSummaryDTO> getChapter(ChapterFilterDTO filter, Pageable pageable) {
+        return chapterService.getChapters(filter, pageable);
     }
 
     @GetMapping(path = "/{chapterUUID}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Chapter> getChapterUUID(@PathVariable UUID chapterUUID) {
-        return ResponseEntity.of(java.util.Optional.ofNullable(chapterService.getChapterUUID(chapterUUID)));
+    public ChapterSummaryDTO getChapterUUID(@PathVariable UUID chapterUUID) {
+        return chapterService.getChapterUUID(chapterUUID);
     }
 
     @PostMapping
