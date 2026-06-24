3991cfe7f7c96f83c29a8123143981ad71c6816d
damouu <mouadsehbaoui@gmail.com>
2026-06-24 15:48:58 +0900

    Refactor: GlobalExceptionHandler

--- src/main/java/com/example/demo/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/handler/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.demo.handler;
 
+import com.example.demo.exception.ChapterAlreadyRegisteredException;
 import com.example.demo.exception.ChapterNotFoundException;
+import com.example.demo.exception.SeriesAlreadyRegisteredException;
+import com.example.demo.exception.SeriesNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,8 +11,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(ChapterNotFoundException.class)
     public ResponseEntity<String> handleException(ChapterNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ChapterAlreadyRegisteredException.class)
+    public ResponseEntity<String> handleException(ChapterAlreadyRegisteredException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(SeriesAlreadyRegisteredException.class)
+    public ResponseEntity<String> handleException(SeriesAlreadyRegisteredException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(SeriesNotFoundException.class)
+    public ResponseEntity<String> handleException(SeriesNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
