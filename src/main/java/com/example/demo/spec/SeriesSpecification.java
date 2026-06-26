e4a216ae3dea0cb88d495a55a2c9073cfbb20c9c
damouu <mouadsehbaoui@gmail.com>
2026-06-26 13:39:42 +0900

    Create: SeriesSpecification (#LACE-23)

--- src/main/java/com/example/demo/spec/SeriesSpecification.java
+++ src/main/java/com/example/demo/spec/SeriesSpecification.java
@@ -2,24 +2,18 @@ package com.example.demo.spec;
 
 import com.example.demo.dto.SeriesFilterDTO;
 import com.example.demo.model.Series;
-import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 
 public class SeriesSpecification {
 
     public static Specification<Series> filterSeries(SeriesFilterDTO filter) {
-        return Specification.where(optionalContains("title", filter.title())).and(optionalContains("author", filter.author())).and(optionalContains("genre", filter.genre())).and(optionalContains("illustrator", filter.illustrator())).and(optionalContains("publisher", filter.publisher()));
+        return Specification
+                .where(SpecificationUtils.<Series>isNull("deletedAt"))
+                .and(SpecificationUtils.optionalContains("title", filter.title()))
+                .and(SpecificationUtils.optionalContains("author", filter.author()))
+                .and(SpecificationUtils.optionalContains("genre", filter.genre()))
+                .and(SpecificationUtils.optionalContains("illustrator", filter.illustrator()))
+                .and(SpecificationUtils.optionalContains("publisher", filter.publisher()));
     }
 
-    private static Specification<Series> optionalContains(String field, String value) {
-
-        if (StringUtils.isBlank(value)) {
-            return null;
-        }
-        return contains(field, value);
-    }
-
-    public static Specification<Series> contains(String field, String value) {
-        return (root, query, cb) -> cb.like(cb.lower(root.get(field)), "%" + value.toLowerCase() + "%");
-    }
 }
